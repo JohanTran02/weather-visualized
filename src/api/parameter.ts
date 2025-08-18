b6fc46cd05c1fa2fb8c1134766e2594fc85c93e3
JohanTran <johan.tran0210@gmail.com>
2025-08-18 12:44:35 +0200

    feat(api): Update getParameters function to return parsed VersionType

--- src/api/parameter.ts
+++ src/api/parameter.ts
@@ -1,17 +1,18 @@
+import { VersionSchema } from '@/types/parameter';
+import { type VersionType } from '@/types/parameter';
 import * as v from 'valibot'
-// import { MetObsStationSetDataTypeSchema, type MetObsStationSetDataType } from '@/types/weather';
 
-export const getStationSet = async () => {
+//Can perhaps create types and use them in the api files?
+export const getParameters = async (): Promise<VersionType> => {
     try {
         const response = await fetch('https://opendata-download-metobs.smhi.se/api/version/1.0.json');
 
         if (!response.ok) throw new Error(`HTTP error ${response.status}`)
 
         const data = await response.json();
-        // const result = v.parse(MetObsStationSetDataTypeSchema, data)
-        // console.log(result);
+        const result = v.parse(VersionSchema, data)
 
-        return data;
+        return result;
     }
     catch (error) {
         console.error(JSON.stringify(error, null, 2));
