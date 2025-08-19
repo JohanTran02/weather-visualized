37d712c31e7cbefae502bd061203d9f62dd37618
JohanTran <johan.tran0210@gmail.com>
2025-08-19 13:58:22 +0200

    feat(routes): Refactor routing structure by removing weather route and integrating map functionality into home component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,25 @@
-import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { MapContainer, TileLayer } from 'react-leaflet'
+import { StationsLayer } from '@/components/StationsLayer'
+import { ParameterList } from '@/components/ParameterList'
+import { useState } from "react";
 
 export const Route = createFileRoute("/")({
   component: HomeComponent,
 });
 
 function HomeComponent() {
+  const [parameterId, setParameterId] = useState<string>('');
   return (
-    <div className="container mx-auto max-w-3xl px-4 py-2">
-      <div>home</div>
+    <div className='relative h-screen w-screen'>
+      <ParameterList parameterId={parameterId} setParameterId={setParameterId} />
+      <MapContainer bounds={[[55, 10], [70, 25]]} zoom={13} scrollWheelZoom={false} style={{ height: '100vh', width: '100%' }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <StationsLayer parameterId={parameterId} />
+      </MapContainer>
     </div>
   );
 }
