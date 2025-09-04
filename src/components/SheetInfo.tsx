de0c0a06f99286b6ece1e5ca32b8eeea10f53dcc
JohanTran <johan.tran0210@gmail.com>
2025-09-04 13:31:11 +0200

    feat(sheet):Add more station info in the sheet

--- src/components/SheetInfo.tsx
+++ src/components/SheetInfo.tsx
@@ -1,5 +1,5 @@
 import type { Dispatch, SetStateAction } from "react";
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "./ui/sheet";
+import { Sheet, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle } from "./ui/sheet";
 import type { StationDataType } from "@/types/station";
 
 export default function SheetInfo({ sheetOpen, setSheetOpen, station }: { sheetOpen: boolean, setSheetOpen: Dispatch<SetStateAction<boolean>>, station: StationDataType | null }) {
@@ -10,11 +10,12 @@ export default function SheetInfo({ sheetOpen, setSheetOpen, station }: { sheetO
             <SheetContent>
                 <SheetHeader>
                     <SheetTitle>{station.name}</SheetTitle>
-                    <SheetDescription>{station.owner}</SheetDescription>
+                    <SheetDescription>{`${station.owner}'s measuring station`}</SheetDescription>
                     <p>Latitude: {station.latitude}°</p>
                     <p>Longitude: {station.longitude}°</p>
                     <p>Altitude: {station.height}m</p>
                 </SheetHeader>
+                <SheetFooter>Based on data from SMHI</SheetFooter>
             </SheetContent>
         </Sheet>
     )
