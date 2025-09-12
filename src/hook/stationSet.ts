3880a7b99c157f65165426aa9488da3a85e09573
JohanTran <johan.tran0210@gmail.com>
2025-09-12 15:55:52 +0200

    fix(station):Create a helper function that determines the sampling type based on station data.

---
+++ src/hook/stationSet.ts
@@ -0,0 +1,53 @@
+import { type StationData, type StationSampling, StationSamplingArraySchema } from "@/types/station";
+import type { MetObsValueType } from "@/types/weather";
+import { useMemo } from "react";
+import { safeParse } from "valibot";
+
+interface ActiveStationsResult {
+    activeStations: StationData[];
+    samplingType: MetObsValueType | null;
+}
+
+interface NormalizedStationResult {
+    stations: StationData[],
+    samplingType: MetObsValueType | null
+}
+
+function isSamplingStation(station: StationData[] | StationData): station is StationSampling[] {
+    const result = safeParse(StationSamplingArraySchema, station);
+    return result.success;
+}
+
+/**
+ * Helper function that normalizes a station or array of stations and determines their sampling type.
+ *
+ * @param {StationData | StationData[]} [station] - A single station or an array of stations. Optional.
+ * @returns {NormalizedStationResult} The normalized stations and their sampling type.
+ */
+export function checkStationType(
+    station?: StationData[] | StationData
+): NormalizedStationResult {
+    if (!station) return { stations: [], samplingType: null };
+
+    const normalizedStations = Array.isArray(station) ? station : [station];
+
+    if (normalizedStations.length === 0) {
+        return { stations: [], samplingType: null };
+    }
+
+    return {
+        stations: normalizedStations,
+        samplingType: isSamplingStation(normalizedStations) ? "SAMPLING" : "INTERVAL",
+    };
+}
+
+export function useGetActiveStationSet(
+    data: StationData[] | undefined
+): ActiveStationsResult {
+
+    return useMemo(() => {
+        const { stations, samplingType } = checkStationType(data);
+
+        return { activeStations: stations.filter((station) => station.value.length), samplingType: samplingType };
+    }, [data]);
+}
