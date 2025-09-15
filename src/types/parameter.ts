2551b3d9403046ead10052664b170fcb561b2216
JohanTran <johan.tran0210@gmail.com>
2025-09-15 14:04:50 +0200

    chore:Remove unused types and files

--- src/types/parameter.ts
+++ src/types/parameter.ts
@@ -1,8 +1,4 @@
-import type { MetObsValueTypeSchema, VersionSchema, MetObsParameterSchema } from "@/schemas/parameter";
+import type { MetObsValueTypeSchema } from "@/schemas/parameter";
 import * as v from "valibot"
 
 export type MetObsValueType = v.InferInput<typeof MetObsValueTypeSchema>;
-
-export type VersionType = v.InferOutput<typeof VersionSchema>
-
-export type MetObsParameter = v.InferOutput<typeof MetObsParameterSchema>
