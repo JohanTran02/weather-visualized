a6c573cdf87f591111027bbe6217bb3d9990525c
JohanTran <johan.tran0210@gmail.com>
2025-09-15 14:46:09 +0200

    refactor(unit):Refactor the unit type and schema

--- src/types/unit.ts
+++
@@ -1,4 +0,0 @@
-import type { UnitKeySchema } from "@/schemas/unit";
-import * as v from "valibot"
-
-export type UnitKey = v.InferOutput<typeof UnitKeySchema>;
--- src/utils/unit.ts
+++ src/utils/unit.ts
@@ -1,5 +1,24 @@
-import { unit } from "@/schemas/unit";
-import { type UnitKey } from "@/types/unit";
+import * as v from 'valibot'
+
+export const unit = {
+    'meter per sekund': 'm/s',
+    'celsius': '°C',
+    'watt per kvadratmeter': 'W/m²',
+    'hektopascal': 'hPa',
+    'kod': '',
+    'meter': 'm',
+    'millimeter per sekund': 'mm/s',
+    'millimeter': 'mm',
+    'procent': '%',
+    'sekund': 's',
+    'grader': '°'
+} as const;
+
+export const UnitKeySchema = v.picklist(
+    Object.keys(unit) as (keyof typeof unit)[]
+);
+
+export type UnitKey = v.InferOutput<typeof UnitKeySchema>;
 
 export function convertUnit(value: string, unitType: UnitKey) {
     return `${value} ${unit[unitType]}`;
