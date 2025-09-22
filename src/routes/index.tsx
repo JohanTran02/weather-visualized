9a1e73a8cb57986e23382a13a4df8473c4f676ee
JohanTran <johan.tran0210@gmail.com>
2025-09-22 15:25:12 +0200

    refactor(map):Refactor map and layers into components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet'
-import { StationsLayer } from '@/components/StationsLayer'
 import { ParameterSelect } from '@/components/ParameterSelect'
 import SheetInfo from "@/components/SheetInfo";
 import { UnitProvider } from "@/context/useUnitContext";
 import { ParameterProvider } from "@/context/useParameterContext";
 import { SheetProvider } from "@/context/useSheetContext";
 import { StationProvider } from "@/context/useStationContext";
+import Map from "@/components/Map";
 
 export const Route = createFileRoute("/")({
   component: HomeComponent,
@@ -21,14 +20,7 @@ function HomeComponent() {
             <div className='relative h-screen w-screen'>
               <SheetInfo />
               <ParameterSelect />
-              <MapContainer className="z-0" bounds={[[55, 10], [70, 25]]} zoom={13} scrollWheelZoom={false} zoomControl={false} style={{ height: '100vh', width: '100%' }}>
-                <TileLayer
-                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <ZoomControl position="topright" />
-                <StationsLayer />
-              </MapContainer>
+              <Map />
             </div>
           </StationProvider>
         </SheetProvider>
