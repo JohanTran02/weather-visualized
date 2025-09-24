2ed39c2d75b975c66c096bf5e3fc7ec6fb350dc3
JohanTran <johan.tran0210@gmail.com>
2025-09-24 18:13:02 +0200

    feat(chart):Sheet now renders data from station using Recharts

--- src/components/Stations.tsx
+++ src/components/Stations.tsx
@@ -1,8 +1,8 @@
-import type { StationData } from "@/types/station";
+import type { StationSetData } from "@/types/station";
 import { FeatureGroup } from 'react-leaflet';
 import StationMarker from "./StationMarker";
 
-export default function Stations({ stations }: { stations: StationData[] }) {
+export default function Stations({ stations }: { stations: StationSetData[] }) {
     return (
         <FeatureGroup>
             {stations.map((station) => (
